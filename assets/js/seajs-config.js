f9f4b1163ae5dcf2835896c1aba610bac6e45ead
DreamerKing <dreamingking@live.cn>
2016-04-26 18:55:40 +0800

    seajs加载

---
+++ assets/js/seajs-config.js
@@ -0,0 +1,8 @@
+seajs.config({
+	base : 'assets/',
+	alias : {
+		'videojs' : 'js/lib/video.js',
+		'videjs-ie8' : 'js/lib/videojs-ie8.js',
+		'video-css' : 'css/video-js.css'
+	}
+})
---
+++ seajs-config.js
@@ -0,0 +1,10 @@
+seajs.config({
+	base : './assets/',
+	alias : {
+		'jquery' : 'js/lib/jquery.js',
+		'videojs' : 'js/lib/video.js',
+		'zh' : 'js/lib/zh-CN.js',
+		'videjs-ie8' : 'js/lib/videojs-ie8.js',
+		'video-css' : 'css/video-js.css'
+	}
+})
